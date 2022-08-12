02fe6e431aa61702cd08affc6cf82c6f436d2b39
VladimirKislov <n_70@inbox.ru>
2022-08-12 10:43:36 +0300

    port heroku

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,7 @@ import { App } from '../App';
 import { indexTemplate } from './indexTemplate';
 import axios from 'axios';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app =express();
 
