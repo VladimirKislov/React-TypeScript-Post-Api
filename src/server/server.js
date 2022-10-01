113a4f99be3f982b0623650d49d2b2520fc233c5
VladimirKislov <n_70@inbox.ru>
2022-10-01 19:49:03 +0400

    fix client

--- src/server/server.js
+++ src/server/server.js
@@ -16,14 +16,14 @@ app.use(helmet({
     contentSecurityPolicy: false,
     crossOriginEmbedderPolicy: false,
 }));
-
+//LTCW2f7TVj4zTVCfmNFpoA
 app.get('/auth', (req, res) => {
     axios
       .post(
         "https://www.reddit.com/api/v1/access_token",
         `grant_type=authorization_code&code=${req.query.code}&redirect_uri=https://my-steps-react-app.herokuapp.com/auth`,
         {
-          auth: { username: "LTCW2f7TVj4zTVCfmNFpoA", password: "d1Wzwk2GH2_YffMsnVnTrRNXhQLuWA" },
+          auth: { username: "UNY4hFpjUAyDW392pyM04g", password: "d1Wzwk2GH2_YffMsnVnTrRNXhQLuWA" },
           headers: { "Content-type": "application/x-www-form-urlencoded" },
         }
       )
--- src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
+++ src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
@@ -11,22 +11,15 @@ interface IUserBlockProps {
 export function UserBlock({ avatarSrc, username, loading }: IUserBlockProps) {
   return (
     <a
-      href={`https://www.reddit.com/api/v1/authorize?client_id=LTCW2f7TVj4zTVCfmNFpoA&response_type=code&state=random_string&redirect_uri=http://localhost:3000/auth&scope=read submit identity`}
+      href={`https://www.reddit.com/api/v1/authorize?client_id=LTCW2f7TVj4zTVCfmNFpoA&response_type=code&state=random_string&redirect_uri=https://my-steps-react-app.herokuapp.com/auth&scope=read submit identity`}
       className={styles.userBox}
     >
       <div className={styles.avatarBox}>
-        { avatarSrc
-          ? <img src={avatarSrc} alt="user_avatar" className={styles.avatarImage} />
-          : <IconAnon />
-        }
-        
+        {avatarSrc ? <img src={avatarSrc} alt="user_avatar" className={styles.avatarImage} /> : <IconAnon />}
       </div>
 
       <div className={styles.username}>
-        {loading
-          ? <span>{'Загрузка...'}</span>
-          : <span>{username || 'Аноним'}</span>
-        }
+        {loading ? <span>{"Загрузка..."}</span> : <span>{username || "Аноним"}</span>}
       </div>
     </a>
   );
